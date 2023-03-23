d230febe40fbbc1dd2c5c635385f6a67d99edcca
YeosCRN <jeoscrn20@outlook.com>
2023-03-23 14:08:09 -0500

    rest

--- models/user.py
+++ models/user.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
-from os import getenv
-
-env = getenv('HBNB_TYPE_STORAGE')
+import os
 
 
-class User(BaseModel, Base if (env == "db") else object):
-    """This class defines a user by various attributes"""
-    if env == "db":
-        __tablename__ = 'users'
+class User(BaseModel, Base):
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="delete")
-        reviews = relationship("Review", backref="user", cascade="delete")
+        places = relationship("Place", passive_deletes=True, backref="user")
+        reviews = relationship("Review", passive_deletes=True, backref="user")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
