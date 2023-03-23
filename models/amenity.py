786cfc8cfcc9227bf6bc96812ada2a04552052dd
chrs-creyk <chrisacount117@gmail.com>
2023-03-23 13:48:24 -0500

    update console

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from os import getenv
 from sqlalchemy.orm import relationship
+from os import getenv
+
+env = getenv('HBNB_TYPE_STORAGE')
 
 
-class Amenity(BaseModel, Base):
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
+class Amenity(BaseModel, Base if (env == "db") else object):
+    """Amenity Class for HBnB"""
+    if env == "db":
+        __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
     else:
         name = ""
