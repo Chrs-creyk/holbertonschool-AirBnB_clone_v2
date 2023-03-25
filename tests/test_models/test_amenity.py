56a42366786c514d22c4642874452d3929596cd7
chrs-creyk <chrisacount117@gmail.com>
2023-03-25 10:45:48 -0500

    test upgrade

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ """
-
-
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """Test Case"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """attribute initialization"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """checks if the name attribute is of type str"""
         new = self.value()
         self.assertEqual(type(new.name), str)
