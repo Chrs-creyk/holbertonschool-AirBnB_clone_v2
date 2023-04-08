ce90829ba1e336b03dedb1b49b9517bc5c77ac22
chrs-creyk <chrisacount117@gmail.com>
2023-04-07 21:01:02 -0500

    adding a list of states in html

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' starts a Flask web application'''
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def st_list():
+    st = storage.all(State).values()
+    return render_template('7-states_list.html', st=st)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''remove the current SQLAlchemy Session'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
