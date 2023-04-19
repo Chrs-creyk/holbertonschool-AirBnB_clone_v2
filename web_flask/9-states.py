68251dea402b7ea7b60fe3afe05c51f49564f704
Chrs-creyk <chrisacount117@gmail.com>
2023-04-19 16:40:56 -0500

    upgrade states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""First flask module for HBNB"""
 
+"""First flask module for HBNB"""
 
 from flask import Flask, render_template
 from models import storage
@@ -9,12 +9,10 @@ from os import getenv
 app = Flask(__name__)
 env = getenv('HBNB_TYPE_STORAGE')
 
-
 @app.teardown_appcontext
 def teardown_close(self):
     "Closses sqlalchemy session"
     storage.close()
-
 
 @app.route('/states/', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
@@ -27,7 +25,6 @@ def state_list(id=None):
         if state.id == id:
             return render_template('9-states.html', st_list=[state], status=2)
     return render_template('9-states.html', st_list=None, status=0)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
